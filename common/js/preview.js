637aa303bc9882bba86302778c95fcf8508ac3ea
WeiChun <weichun@csie.tw>
2017-05-30 13:10:10 +0800

    add view by hash

--- common/js/preview.js
+++ common/js/preview.js
@@ -1,7 +1,10 @@
 $(function () {
     "use strict";
+    var hashFromClick = false;
     $("#demoLinks a").click(function (e) {
         var href = this.href;
+        hashFromClick = true;
+        location.hash = this.getAttribute('href');
         $.ajax(href, {
             type: "GET",
             cache: true,
@@ -27,4 +30,26 @@ $(function () {
 
         e.preventDefault();
     });
+
+    function handleHashChange() {
+        if (hashFromClick) {
+            hashFromClick = false;
+            return;
+        }
+
+        var hash = encodeURIComponent(location.hash.substr(1));
+        var a = $("a[href$='" + hash + "']");
+        if(hash.length===0){
+        }else if(a.length===1){
+            a.click();
+        }else{
+            $("#source, #preview").empty();
+            location.hash = "";
+        }
+    }
+
+    $(window).bind('hashchange', function (e) {
+        handleHashChange();
+    });
+    handleHashChange();
 });
